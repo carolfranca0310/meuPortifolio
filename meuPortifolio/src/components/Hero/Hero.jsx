6cb91fe9161be52d7214386888c1154520bee78c
Caroline Araujo de Franca <caroline.franca@grendene.com.br>
2024-01-18 08:53:30 -0300

    feat: feita a responsividade do Hero; trocada a paleta de cores

--- meuPortifolio/src/components/Hero/Hero.jsx
+++ meuPortifolio/src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
     return (
         <section className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>lorem ipsulum!</h1>
-                <p className={styles.description}>Sou graduanda de Engenharia de Software, tenho 23 anos e sou desenvolvedora fullstack. </p>
+                <h1 className={styles.title}>Olá, eu sou Caroline!</h1>
+                <p className={styles.description}>Breve descrição</p>
                 <a className={styles.contactBtn} href="mailto:carolfranca0310@gmail.com">Entre em contato!</a>
             </div>
             <img src={getImageUrl("hero/heroImage.png")} alt="Imagem de mim" className={styles.heroImg} />
