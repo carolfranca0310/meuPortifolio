0c7bcce92ad401798763a64fd4a200fe10a48a43
Caroline Araujo de Franca <caroline.franca@grendene.com.br>
2024-01-19 13:53:38 -0300

    feat: criação e estilização da section projects assim como a criação do arquivo json contendo os projetos

--- meuPortifolio/src/components/Hero/Hero.jsx
+++ meuPortifolio/src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
                 <p className={styles.description}>Breve descrição</p>
                 <a className={styles.contactBtn} href="mailto:carolfranca0310@gmail.com">Entre em contato!</a>
             </div>
-            <img src={getImageUrl("hero/heroImage.png")} alt="Imagem de mim" className={styles.heroImg} />
+            <img src={getImageUrl("hero/heroImage2.png")} alt="Imagem de mim" className={styles.heroImg} />
             <div className={styles.topBlur} />
             <div className={styles.bottomBlur} />
         </section>
